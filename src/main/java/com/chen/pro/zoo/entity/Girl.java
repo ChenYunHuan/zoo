bd5fdb2e0b6b2992555367b1b20bb2df08999d1c
CHENYUNHUAN <1589817758@qq.com>
2019-10-09 16:40:17 +0800

    添加对数据库增删改查操作

--- src/main/java/com/chen/pro/zoo/entity/Girl.java
+++ src/main/java/com/chen/pro/zoo/entity/Girl.java
@@ -1,7 +1,19 @@
 package com.chen.pro.zoo.entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
+@Data
 public class Girl {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String size;
+
+    private Integer age;
 }
